040951adced3625f4644731acd2af4ab0af1106f
ljh7879 <89470177+ljh7879@users.noreply.github.com>
2021-09-04 19:26:40 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -8,7 +8,6 @@ sec_key = "ad9108675991fcaeb3b8b433dbe0a33b"
 myToken = "xoxb-2419582512212-2419602096964-YEyaeOvAy5eZd0cxskiPIMX4"
 
 def post_message(token, channel, text):
-    """슬랙 메시지 전송"""
     response = requests.post("https://slack.com/api/chat.postMessage",
         headers={"Authorization": "Bearer "+token},
         data={"channel": channel,"text": text}
@@ -19,12 +18,11 @@ bithumb = pybithumb.Bithumb(con_key, sec_key)
 
 def get_target_price(ticker):
     df = pybithumb.get_ohlcv(ticker)
-    yesterday = df.iloc[-2]
-
+    yesterday = df.loc[yesterday_check]
     today_open = yesterday['close']
     yesterday_high = yesterday['high']
     yesterday_low = yesterday['low']
-    target = today_open + (yesterday_high - yesterday_low) * 0.7
+    target = today_open + (yesterday_high - yesterday_low) * 0.2
     return target
 
 def buy_crypto_currency(ticker):
@@ -46,8 +44,9 @@ def get_yesterday_ma5(ticker):
 
 now = datetime.datetime.now()
 mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
-ma5 = get_yesterday_ma5("BTC")
-target_price = get_target_price("BTC")
+yesterday_check = datetime.datetime(now.year, now.month, now.day) - datetime.timedelta(1)
+ma5 = get_yesterday_ma5("ETH")
+target_price = get_target_price("ETH")
 post_message(myToken,"#engineer", "autotrade start")
 
 while True:
@@ -55,16 +54,17 @@ while True:
         now = datetime.datetime.now()
         print(now)
         if mid < now < mid + datetime.delta(seconds=10): 
-            target_price = get_target_price("BTC")
+            target_price = get_target_price("ETH")
             mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
-            ma5 = get_yesterday_ma5("BTC")
-            sell_crypto_currency("BTC")
-            post_message(myToken,"#engineer", "BTC sell completed")
+            ma5 = get_yesterday_ma5("ETH")
+            sell_crypto_currency("ETH")
+            post_message(myToken,"#engineer", "Coin sell completed")
     
         current_price = pybithumb.get_current_price("BTC")          
         if (current_price > target_price) and (current_price > ma5):
-            buy_crypto_currency("BTC")
-            post_message(myToken,"#engineer", "BTC buy completed") 
+            if (now > mid + datetime.timedelta(minutes=2)):
+                buy_crypto_currency("ETH")
+                post_message(myToken,"#engineer", "Coin buy completed" + "target price : " + str(target_price)) 
 
     except:
         print("에러 발생")        
